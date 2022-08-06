7fd4c50573012d37c3fe1a5501728810c1863b98
Kyle <ky000le@163.com>
2022-08-06 14:41:07 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,8 +9,8 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Kyle's Space',
-  domain: 'Kylespace',
+  name: 'Kyle Space',
+  domain: 'kylespace.vercel.app',
   author: 'Kyle',
 
   // open graph metadata (optional)
